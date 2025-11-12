3a4cce8c01ddbb06bf6ee3851d34cae4f63984e3
AbuJubaeid <abujubaeidpranto2@gmail.com>
2025-11-12 17:14:29 +0600

    delete a product function added

--- index.js
+++ index.js
@@ -62,6 +62,14 @@ async function run() {
       res.send(result);
     });
 
+     // delete a product
+    app.delete("/myExports/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productsCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // add data
     app.post('/products', async (req, res)=>{
         const newProduct = req.body;
