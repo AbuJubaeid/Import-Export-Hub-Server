5ffcd88b3f024644cc577088fd93dd34428ad472
AbuJubaeid <abujubaeidpranto2@gmail.com>
2025-11-13 15:06:46 +0600

    revised

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const db = client.db('IEHub-db')
     const productsCollection = db.collection('products')
@@ -78,13 +78,6 @@ async function run() {
           const id = req.params.id;
           const query = { _id: new ObjectId(id) };
           const result = await productsCollection.findOne(query);
-          res.send(result);
-        });
-
-        app.get("/myImports/:id",verifyFirebaseToken, async (req, res) => {
-          const id = req.params.id;
-          const query = { _id: new ObjectId(id) };
-          const result = await importsCollection.findOne(query);
           res.send(result);
         });
 
@@ -189,7 +182,7 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
